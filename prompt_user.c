a0ca0d84e84e17e00da3faab753049dcf4dbbd81
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-07-04 14:21:28 +0000

    updated promptfile

--- prompt_user.c
+++ prompt_user.c
@@ -17,10 +17,7 @@ void prompt_user(char **av, char **envt)
 			printf("cisfun$ ");
 		ssize_t char_num = getline(&str, &n, stdin);
 		if (char_num == -1)
-		{
-			free(str);
-			exit(EXIT_FAILURE);
-		}
+			free(str), exit(EXIT_FAILURE);
 		i = 0;
 		while (str[i])
 		{
@@ -31,14 +28,10 @@ void prompt_user(char **av, char **envt)
 		argv[0] = str;
 		child_pid = fork();
 		if (child_pid == -1)
-		{
-			free(str);
-			exit(EXIT_FAILURE);
-		}
+			free(str), exit(EXIT_FAILURE);
 		if (child_pid == 0)
 		{
-			int n = execve(argv[0], argv, envt);
-			if (n == -1)
+			if (execve(argv[0], argv, envt) == -1);
 				printf("%s, no such file or directory\n", av[0]);
 		}
 		else
