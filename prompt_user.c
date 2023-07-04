180e0373bba7526eadf72c6247dc695d4241b674
Toyin Obafemi <byhisgraceabba@gmail.com>
2023-07-04 13:49:08 +0000

    file to prompt user on the simple shell

---
+++ prompt_user.c
@@ -0,0 +1,47 @@
+#include "simple_shell.h"
+/**
+ *
+ */
+
+void prompt_user(char **av, char **envt)
+{
+	char *str = NULL;
+	size_t n = 0;
+	int i, status;
+	char *argv[] = {NULL, NULL};
+	pid_t child_pid;
+
+	while (1)
+	{
+		if (isatty(fileno(stdin)))
+			printf("cisfun$ ");
+		ssize_t char_num = getline(&str, &n, stdin);
+		if (char_num == -1)
+		{
+			free(str);
+			exit(EXIT_FAILURE);
+		}
+		i = 0;
+		while (str[i])
+		{
+			if (str[i] == '\n')
+				str[i] = 0;
+			i++;
+		}
+		argv[0] = str;
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			free(str);
+			exit(EXIT_FAILURE);
+		}
+		if (child_pid == 0)
+		{
+			int n = execve(argv[0], argv, envt);
+			if (n == -1)
+				printf("%s, no such file or directory\n", av[0]);
+		}
+		else
+			wait(&status);
+	}
+}
